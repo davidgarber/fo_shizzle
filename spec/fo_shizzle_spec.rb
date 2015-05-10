cdb7957a30105ed183f6071763d9918c44ccef7a
David Garber <davidgarber@gmail.com>
2015-05-09 21:00:36 -0700

    does not change the letter 'S' to 'Z; if it is a capital.

--- spec/fo_shizzle_spec.rb
+++ spec/fo_shizzle_spec.rb
@@ -5,4 +5,8 @@ describe('fo_shizzle') do
   it("changes the letter 's' to a letter 'z'") do
     expect(("bets").fo_shizzle).to(eq("betz"))
   end
+
+  it("does not change the letter 'S' to 'z' if it is a capital") do
+    expect(("Susan").fo_shizzle).to(eq("Suzan"))
+  end
 end
