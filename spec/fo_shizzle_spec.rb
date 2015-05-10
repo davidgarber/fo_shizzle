71ac97194269f0e6b19617f985d8f8e53a00765c
David Garber <davidgarber@gmail.com>
2015-05-09 21:06:10 -0700

    does not change the letter 's' if it is the first letter.

--- spec/fo_shizzle_spec.rb
+++ spec/fo_shizzle_spec.rb
@@ -9,4 +9,8 @@ describe('fo_shizzle') do
   it("does not change the letter 'S' to 'z' if it is a capital") do
     expect(("Susan").fo_shizzle).to(eq("Suzan"))
   end
+
+  it("does not change the letter 's' to 'z' if it is the first letter") do
+    expect(("I said hers").fo_shizzle).to(eq("I said herz"))
+  end
 end
